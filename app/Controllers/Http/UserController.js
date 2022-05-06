7dd9778bf1c0b0fa155b2ac0efdd65fbd0af60c5
Ferquivi <102878032+Ferquivi@users.noreply.github.com>
2022-05-06 15:37:46 -0500

    confirmar

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class UserController {
+store(){
+    return{
+        mensaje: ' creamos un usuario desde el controlador'
+    }
+}
+}
+
+module.exports = UserController
---
+++ start/routes.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const UserController = require('../app/Controllers/Http/UserController')
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
+
+Route.post('usuarios/registro', 'UserController.store');
